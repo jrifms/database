29be9a0ed2fe730abb9799041dfe290de64e50b7
Junior <estrutura.pilha@gmail.com>
2023-05-19 14:22:13 -0400

    Atualizando com uma documentação mais fundamentada.

--- mysql/serverIfms.py
+++ mysql/serverIfms.py
@@ -4,14 +4,27 @@ from conexao import Conexao
 import signal
 
 class PythonProcessing:
+    """
+    Classe responsável pelo processamento do arquivo e execução de consultas SQL.
+    """
 
-    def arquivo(self,nome='index.html', query='select * from venda'):
+    def arquivo(self, nome='index.html', query='select * from venda'):
+        """
+        Processa o arquivo especificado, substituindo a tag '<ColocarTabelaAqui>' pelo resultado de uma consulta SQL.
+
+        Parâmetros:
+        - nome (str): O nome do arquivo a ser processado. O padrão é 'index.html'.
+        - query (str): A consulta SQL a ser executada. O padrão é 'select * from venda'.
+
+        Retorna:
+        - str: O conteúdo do arquivo processado com a tabela substituída pelos resultados da consulta SQL.
+        """
         try:
             conexao = Conexao()
-            tabela  = conexao.get_data(sql=query)
+            tabela = conexao.get_data(sql=query)
             conexao.close()
             lines = ''
-            with open(nome,'r') as file:
+            with open(nome, 'r') as file:
                 for line in file:
                     line = line.replace('<ColocarTabelaAqui>', tabela)
                     lines += str(line)
@@ -19,16 +32,35 @@ class PythonProcessing:
         except ValueError as error:
             print(error)
 
+
 class GerenciaRequisicao(http.server.SimpleHTTPRequestHandler):
+    """
+    Classe responsável por gerenciar as requisições HTTP e enviar as respostas correspondentes.
+    """
+
     def do_GET(self):
+        """
+        Manipula a requisição GET, enviando o arquivo processado como resposta.
+        """
         file = PythonProcessing()
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(file.arquivo().encode())
-        
+
+
 class ServidorIFMS:
+    """
+    Classe responsável por iniciar o servidor web e tratar o encerramento correto.
+    """
+
     def __init__(self):
+        """
+        Inicializa uma nova instância do servidor IFMS.
+
+        Inicia o servidor web na porta 9009, gerenciando as requisições com a classe GerenciaRequisicao.
+        Registra um manipulador de sinal para lidar com o encerramento correto do servidor.
+        """
         try:
             port = 9009
             handler = GerenciaRequisicao
@@ -38,8 +70,15 @@ class ServidorIFMS:
                 self.server.serve_forever()
         except ValueError as error:
             print(error)
-    # Fechar o servidor web (Ctrl+C)
-    def shutdown_handler(self,signal, frame):
+
+    def shutdown_handler(self, signal, frame):
+        """
+        Manipulador de sinal para encerrar corretamente o servidor.
+
+        Parâmetros:
+        - signal: O sinal recebido.
+        - frame: O quadro atual.
+        """
         print("Fechando o servidor")
         self.server.server_close()
 
